2bdbb58f5a6c6d950f3a3b29511c3f1ebf567ccf
Lucas Melin <lucas.melin@gmail.com>
2023-07-09 22:30:16 -0400

    Improve flake config

--- flake.nix
+++ flake.nix
@@ -1,8 +1,17 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
-  outputs = inp:
-    inp.dream2nix.lib.makeFlakeOutputs {
-      systems = ["aarch64-darwin"];
+  description = "Nix Typescript Obsidian plugin";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    dream2nix.url = "github:nix-community/dream2nix";
+  };
+  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
+    dream2nix.lib.makeFlakeOutputs {
+      systems = flake-utils.lib.defaultSystems;
       config.projectRoot = ./.;
       source = ./.;
       projects = ./projects.toml;
